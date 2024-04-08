daaad9bcd4bb8afd4fb8eed8ae5f85eb1d1a527f
JanvE97 <112380765+JanvE97@users.noreply.github.com>
2024-04-08 08:25:24 +0200

    latest changes impact module + factsheet generation added

--- tuflowflash/impact_module.py
+++ tuflowflash/impact_module.py
@@ -1,12 +1,14 @@
-from rasterstats import zonal_stats
+from rasterstats import zonal_stats, point_query
 import geopandas as gpd
 import rasterio
 from rasterio import features
 import numpy as np
+import pandas as pd
 
 BUFFER_SIZE = 5
 CAP_STYLE = 2
-CLASSES = {"low": 1, "medium": 2, "high": 3}
+CLASSES = {"low": 1, "medium": 2, "high": 3, "very high": 4}
+
 
 def save_array_to_tiff(outfile, array, profile):
     """Saves rasterio or numpy array to tiff file
@@ -19,7 +21,10 @@ def save_array_to_tiff(outfile, array, profile):
     with rasterio.open(outfile, "w", **profile) as dst:
         dst.write(array, 1)
 
-def rasterize_vector_column(geodataframe, value_column, outfile, transform,depth_raster):
+
+def rasterize_vector_column(
+    geodataframe, value_column, outfile, transform, depth_raster
+):
     """Process vector_file to raster
 
     Args:
@@ -34,7 +39,7 @@ def rasterize_vector_column(geodataframe, value_column, outfile, transform,depth
     with rasterio.open(depth_raster) as src:
         shape = src.shape
 
-    ones_list = list(geodataframe[geodataframe[value_column]==1]["geometry"])
+    ones_list = list(geodataframe[geodataframe[value_column] == 1]["geometry"])
     if ones_list:
         features.rasterize(
             ones_list,
@@ -45,7 +50,7 @@ def rasterize_vector_column(geodataframe, value_column, outfile, transform,depth
             default_value=1,
         )
 
-    twos_list = list(geodataframe[geodataframe[value_column]==2]["geometry"])
+    twos_list = list(geodataframe[geodataframe[value_column] == 2]["geometry"])
     if twos_list:
         features.rasterize(
             twos_list,
@@ -56,7 +61,7 @@ def rasterize_vector_column(geodataframe, value_column, outfile, transform,depth
             default_value=2,
         )
 
-    threes_list = list(geodataframe[geodataframe[value_column]==3]["geometry"])
+    threes_list = list(geodataframe[geodataframe[value_column] == 3]["geometry"])
     if threes_list:
         features.rasterize(
             threes_list,
@@ -67,66 +72,206 @@ def rasterize_vector_column(geodataframe, value_column, outfile, transform,depth
             default_value=3,
         )
 
+
 class impactModule:
     def __init__(self, settings, end_result_type):
         self.settings = settings
         self.end_result_type = end_result_type
 
-    def translate_to_classes(self, vector_layer, threshold1, threshold2):
+    def translate_to_classes(self, vector_layer, threshold0, threshold1, threshold2):
         vector_layer["max_depth"].fillna(0, inplace=True)
         vector_layer.loc[
-            vector_layer["max_depth"] < threshold1, ["vulnerability_class"]
+            vector_layer["max_depth"] <= threshold0, ["vulnerability_class"]
         ] = CLASSES["low"]
+
+        vector_layer.loc[
+            (vector_layer["max_depth"] > threshold0)
+            & (vector_layer["max_depth"] < threshold1),
+            ["vulnerability_class"],
+        ] = CLASSES["medium"]
+
         vector_layer.loc[
             (vector_layer["max_depth"] >= threshold1)
             & (vector_layer["max_depth"] < threshold2),
             ["vulnerability_class"],
-        ] = CLASSES["medium"]
+        ] = CLASSES["high"]
+
         vector_layer.loc[
             vector_layer["max_depth"] >= threshold2, ["vulnerability_class"]
-        ] = CLASSES["high"]
+        ] = CLASSES["very high"]
 
         return vector_layer
 
-    def determine_vulnerability_roads(self, geopackage, crs, depth_raster):
-        roads = gpd.read_file(geopackage, crs=crs)
-        geometry_linestrings = roads.geometry
-        # from linestrings to polygons by buffering, one value for each road within the buffered road
-        roads.geometry = roads.geometry.buffer(BUFFER_SIZE, cap_style=CAP_STYLE)
-        with rasterio.open(depth_raster) as src:
-            zonal_statistics = zonal_stats(
-                roads, src.read(1), affine=src.transform, stats=["max", "sum"]
-            )
+    def sample_raster(
+        self,
+        raster_location,
+        shapes,
+        shapes_reference_level_colname,
+        buffer_size=0,
+        fill_value=0,
+    ):
+        with rasterio.open(raster_location) as src:
+            if buffer_size > 0:
+                zonal_statistics = zonal_stats(
+                    shapes.buffer(buffer_size).tolist(),
+                    src.read(1),
+                    affine=src.transform,
+                    stats=["max", "sum"],
+                )
+            else:
+                zonal_statistics = point_query(
+                    shapes["geometry"].tolist()[0], src.read(1), affine=src.transform
+                )
+        shapes = shapes.fillna(value=np.nan)
+        shapes["max_wl"] = list(map(lambda x: x["max"], zonal_statistics))
+        shapes["volume"] = list(map(lambda x: x["sum"], zonal_statistics))
 
-            roads["max_depth"] = list(map(lambda x: x["max"], zonal_statistics))
-            roads["volume"] = list(map(lambda x: x["sum"], zonal_statistics))
+        shapes["max_depth"] = shapes.apply(
+            lambda row: row["max_wl"] - row[shapes_reference_level_colname]
+            if not pd.isnull(row["max_wl"])
+            else fill_value,
+            axis=1,
+        )
+        print(shapes)
+        return shapes
 
-            vulnerable_roads_results = self.translate_to_classes(roads, 0.15, 0.3)
-            if self.end_result_type == "geoserver":
-                vulnerable_roads_results.geometry = geometry_linestrings
-            output_filename = depth_raster.replace(".tif", "_roads.gpkg")
-            vulnerable_roads_results.to_file(
-                str(output_filename), layer="vulnerable_roads"
-            )
+    def determine_floor_level(
+        self, building_shape_path, floor_level_shape_path, floor_level_column
+    ):
+        buildings = gpd.read_file(building_shape_path)
+        floor_level_points = gpd.read_file(floor_level_shape_path)
+
+        # intersect floor level points with building footprints (with small buffer to include)
+        buildings["uuid"] = [f"BUILDING_{int(x)}" for x in range(len(buildings))]
+
+        buildings_buff = buildings.copy()
+        buildings_buff["geometry"] = buildings_buff.buffer(2)
+        buildings_buff_floor_level = buildings_buff.sjoin(
+            floor_level_points[[floor_level_column, "geometry"]],
+            predicate="intersects",
+            how="left",
+        )
+
+        buildings_buff_floor_level_grouped = buildings_buff_floor_level.groupby("uuid")[
+            [floor_level_column]
+        ].min()
+
+        def get_floor_level(row):
+            floor_level = buildings_buff_floor_level_grouped.loc[
+                buildings_buff_floor_level_grouped.index == row.uuid
+            ]
+
+            if np.isnan(floor_level[floor_level_column].iloc[0]):
+                # use dem zonal statistics > min > + 0.15 m for missing values
+                return row.DTM_STATSmin + 0.15
+            else:
+                return floor_level[floor_level_column].iloc[0]
+
+        buildings["floor_level"] = buildings.apply(
+            lambda row: get_floor_level(row), axis=1
+        )
+
+        return buildings
+
+    def determine_vulnerability_buildings(
+        self,
+        buildings_geopackage,
+        waterlevel_raster,
+        reference_level_column_name="Level",
+        buffer_size=2,
+    ):
+        buildings_sampled = self.sample_raster(
+            raster_location=waterlevel_raster,
+            shapes=buildings_geopackage,
+            shapes_reference_level_colname=reference_level_column_name,
+            buffer_size=buffer_size,
+        )
+
+        vulnerable_buildings_results = self.translate_to_classes(
+            buildings_sampled, 0, 0.25, 1
+        )
+
+        output_filename = waterlevel_raster.replace(".tif", "_buildings.gpkg")
+        vulnerable_buildings_results.to_file(
+            str(output_filename), layer="vulnerable_buildings"
+        )
         return output_filename
 
-    def determine_vulnerability_buildings(self, geopackage, crs, depth_raster):
-        buildings = gpd.read_file(geopackage, crs=crs)
-        with rasterio.open(depth_raster) as src:
-            zonal_statistics = zonal_stats(
-                buildings, src.read(1), affine=src.transform, stats=["max", "sum"]
-            )
+    def determine_vulnerability_road_closure_points(
+        self,
+        road_closure_pt_geopackage,
+        waterlevel_raster,
+        buffer_size=10,
+        reference_level_column_name="Level",
+    ):
+        # set fill value to -9999, which means that when no waterlevel is found, the risk is assessed at < -0.3 m WD instead of 0, which would classify this as a medium risk
+        road_closure_points_sampled = self.sample_raster(
+            raster_location=waterlevel_raster,
+            shapes=road_closure_pt_geopackage,
+            shapes_reference_level_colname=reference_level_column_name,
+            buffer_size=buffer_size,
+            fill_value=-9999,
+        )
 
-            buildings["max_depth"] = list(map(lambda x: x["max"], zonal_statistics))
-            buildings["volume"] = list(map(lambda x: x["sum"], zonal_statistics))
+        vulnerable_road_closure_points_results = self.translate_to_classes(
+            road_closure_points_sampled, -0.3, 0, 0.15
+        )
 
-            vulnerable_buildings_results = self.translate_to_classes(
-                buildings, 0.15, 0.3
-            )
-            output_filename = depth_raster.replace(".tif", "_buildings.gpkg")
-            vulnerable_buildings_results.to_file(
-                str(output_filename), layer="vulnerable_buildings"
-            )
+        output_filename = waterlevel_raster.replace(".tif", "_road_closure_points.gpkg")
+        vulnerable_road_closure_points_results.to_file(
+            str(output_filename), layer="vulnerable_road_closure_points"
+        )
+        return output_filename
+
+    def determine_vulnerability_evacuation_centres(
+        self,
+        evac_centre_geopackage,
+        waterlevel_raster,
+        buffer_size=100,
+        reference_level_column_name="Level",
+    ):
+        # set fill value to -9999, which means that when no waterlevel is found, the risk is assessed at < -0.3 m WD instead of 0, which would classify this as a medium risk
+        evacuation_centres_sampled = self.sample_raster(
+            raster_location=waterlevel_raster,
+            shapes=evac_centre_geopackage,
+            shapes_reference_level_colname=reference_level_column_name,
+            buffer_size=buffer_size,
+            fill_value=-9999,
+        )
+
+        vulnerable_evacuation_centres_results = self.translate_to_classes(
+            evacuation_centres_sampled, -0.3, 0, 0.15
+        )
+
+        output_filename = waterlevel_raster.replace(".tif", "_evacuation_centres.gpkg")
+        vulnerable_evacuation_centres_results.to_file(
+            str(output_filename), layer="vulnerable_evacuation_centres"
+        )
+        return output_filename
+
+    def determine_vulnerability_council_assets(
+        self,
+        council_assets_geopackage,
+        waterlevel_raster,
+        buffer_size=2,
+        reference_level_column_name="Level",
+    ):
+        # set fill value to -9999, which means that when no waterlevel is found, the risk is assessed at < -0.3 m WD instead of 0, which would classify this as a medium risk
+        council_assets_sampled = self.sample_raster(
+            raster_location=waterlevel_raster,
+            shapes=council_assets_geopackage,
+            shapes_reference_level_colname=reference_level_column_name,
+            buffer_size=buffer_size,
+            fill_value=-9999,
+        )
+        vulnerable_council_assets_results = self.translate_to_classes(
+            council_assets_sampled, -0.3, 0, 0.15
+        )
+
+        output_filename = waterlevel_raster.replace(".tif", "_council_assets.gpkg")
+        vulnerable_council_assets_results.to_file(
+            str(output_filename), layer="vulnerable_council_assets"
+        )
         return output_filename
 
     def create_impact_raster(self, vector_list, depth_raster):
@@ -145,5 +290,13 @@ class impactModule:
                 transform,
                 depth_raster,
             )
-        save_array_to_tiff(depth_raster.replace(".tif", "_vulnerability.tif"), impact_data, profile)
+        save_array_to_tiff(
+            depth_raster.replace(".tif", "_vulnerability.tif"), impact_data, profile
+        )
         return depth_raster.replace(".tif", "_vulnerability.tif")
+
+    def create_impact_vector(self, layer_dict, impact_vector_output_path):
+        for i, (layer_name, layer_location) in enumerate(layer_dict.items()):
+            points = gpd.read_file(layer_location)
+            points = points[["vulnerability_class", "geometry"]]
+            points.to_file(impact_vector_output_path, driver="GPKG", layer=layer_name)
